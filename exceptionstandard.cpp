3663b795e24c6e1b0bc73b658b434a8481e1e671
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-18 10:17:13 +0700

    streamReaderAndWriter,Deklarasi Variabel Baris untukmmenyimpan data string

---
+++ exceptionstandard.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main() {
+    {
+        cout << "Awal program" << endl; // penanda 1
+        try
+        {
+            // deklarsasi array data dengan panjang index 3
+            array <int , 3> data = {1, 2, 3};
+            // memanggil array element ke-5 yang berarti akan terjadi error
+            cout << data.at(5) << endl;
+        }
+        //kondisi jika terjadi error dalam try
+        catch (exception& e)
+        {
+          
+            cout << e.what() << endl; 
+        }
+    cout << "baris program terkahir" << endl; // penanda 2
+    return 0;
+}
