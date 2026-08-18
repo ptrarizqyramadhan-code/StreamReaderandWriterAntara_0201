67a49d454c04308f9ae367f9176dee67222a2b7b
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-18 10:19:21 +0700

    unlimited loop menulis file

--- StreamReaderAndWriter.cpp
+++ StreamReaderAndWriter.cpp
@@ -14,3 +14,9 @@ int main()
     outfile.open("contohfile.txt");
 
     cout << "=> Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true)
+    {
+        cout << "- ";
+       
