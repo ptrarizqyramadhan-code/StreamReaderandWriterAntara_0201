f7d8d27adc544a5337867ee3a2eca0a70b470178
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-18 10:24:25 +0700

    perulangan untuk memunculkan setiap baris

--- StreamReaderAndWriter.cpp
+++ StreamReaderAndWriter.cpp
@@ -40,4 +40,8 @@ int main()
     //kondisi jika file ada
     if (infile.is_open())
     {
-      
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+       
