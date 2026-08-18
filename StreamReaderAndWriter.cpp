532db50e7dd0b7112659b42a875068db859b9999
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-08-18 10:25:02 +0700

    menutup file jiak telah selesai dan kondisi jika file tidak ada

--- StreamReaderAndWriter.cpp
+++ StreamReaderAndWriter.cpp
@@ -44,4 +44,10 @@ int main()
         while (getline(infile, baris)) {
             cout << baris << endl;
         }
-       
+        //menutup file jika telah selesai
+        infile.close();
+    }
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    return 0;
+}
